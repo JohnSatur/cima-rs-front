ff4f25d9b3588c840b25bc73a81cb6637c30f765
John Satur <animashorse3@yahoo.com.mx>
2025-02-17 19:26:04 -0600

    Colecciones y documentos de prueba creados para el blog

---
+++ src/content.config.ts
@@ -0,0 +1,36 @@
+import { glob } from 'astro/loaders';
+import { defineCollection, reference, z } from 'astro:content';
+
+const postsCollection = defineCollection({
+  loader: glob({ pattern: '**/*.{md,mdx}', base: "./src/content/posts" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+    author: reference('authors'),
+    image: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+    relatedPosts: z.array(reference('posts')).optional(),
+  }),
+});
+
+const authorsCollection = defineCollection({
+  loader: glob({ pattern: '**/*.json', base: "./src/content/authors" }),
+  schema: ({ image }) => z.object({
+    name: z.string(),
+    bio: z.string(),
+    email: z.string().email(),
+    role: z.enum(['Agente - CIMA Bienes Raíces']),
+    headshot: image().optional().default({
+      src: '/images/default-headshot.jpg',
+      width: 280,
+      height: 280,
+      format: 'jpg',
+    }),
+  })
+});
+
+export const collections = {
+  posts: postsCollection,
+  authors: authorsCollection
+};
